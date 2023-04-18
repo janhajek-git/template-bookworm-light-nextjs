78e1053e0c43090d2028dfde6ff4ac9bce884fb4
janhajek-git <mr.janhajek@gmail.com>
2023-04-18 16:55:14 +0300

    added local fonts

--- pages/_app.js
+++ pages/_app.js
@@ -2,23 +2,11 @@ import config from "@config/config.json";
 import theme from "@config/theme.json";
 import { JsonContext } from "context/state";
 import Head from "next/head";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import TagManager from "react-gtm-module";
 import "styles/style.scss";
 
 const App = ({ Component, pageProps }) => {
-  // import google font css
-  const pf = theme.fonts.font_family.primary;
-  const sf = theme.fonts.font_family.secondary;
-  const [fontcss, setFontcss] = useState();
-  useEffect(() => {
-    fetch(
-      `https://fonts.googleapis.com/css2?family=${pf}${
-        sf ? "&family=" + sf : ""
-      }&display=swap`
-    ).then((res) => res.text().then((css) => setFontcss(css)));
-  }, [pf, sf]);
-
   // google tag manager (gtm)
   const tagManagerArgs = {
     gtmId: config.params.tag_manager_id,
@@ -33,17 +21,6 @@ const App = ({ Component, pageProps }) => {
   return (
     <JsonContext>
       <Head>
-        {/* google font css */}
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <style
-          dangerouslySetInnerHTML={{
-            __html: `${fontcss}`,
-          }}
-        />
         {/* responsive meta */}
         <meta
           name="viewport"
